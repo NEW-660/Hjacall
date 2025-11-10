330ca5b607bd153c48c2413aa0ce44c6bf36b0d8
NEW LOOK <124157392+NEW-660@users.noreply.github.com>
2025-11-10 13:41:19 +0200

    Add files via upload

---
+++ الاوامر.py
@@ -0,0 +1,24 @@
+>"اوامر تشغيل اداه سبام المكالمات الطريقه شغاله بنسبه 70%
+
+
+>"اوامر تثبيت المكتبه من 9
+
+1:- pyfiglet
+2:- termcolor
+3:- colored
+4:- requests
+5:- json
+6:- random
+7:- string
+8:- time
+9:- uuid
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+"https://t.me/GalalAlmanse2"
+"https://t.me/Almansi1_bot"
+"https://t.me/GalalAlmansiGM"
+"https://t.me/Almansi2_bot" 
+"https://youtube.com/@almansi12" 
+"https://www.youtube.com/@almansi.2" 
+"https://whatsapp.com/channel/0029Vb5eRwL4NViiThAnsu2h" 
+『𝔸𝕃𝕄𝔸ℕ𝕊𝕀』
+𓄂Almansi𖢳
